ef765b4cd18dee49fc10784b00a19f6142d5aaf6
Austin Malerba <austin.malerba@gmail.com>
2018-12-05 14:04:20 -0600

    Updates

---
+++ precache-manifest.1280f77c33fe226320f12580c1ada1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "133782e56d590fd571a2",
+    "url": "/climax/static/js/runtime~main.133782e5.js"
+  },
+  {
+    "revision": "9cda8e38efb892ff84c6",
+    "url": "/climax/static/js/main.9cda8e38.chunk.js"
+  },
+  {
+    "revision": "7c79798f03b223bab54b",
+    "url": "/climax/static/js/1.7c79798f.chunk.js"
+  },
+  {
+    "revision": "9cda8e38efb892ff84c6",
+    "url": "/climax/static/css/main.fe2888c4.chunk.css"
+  },
+  {
+    "revision": "7c79798f03b223bab54b",
+    "url": "/climax/static/css/1.181480d7.chunk.css"
+  },
+  {
+    "revision": "227fc09bcc1fd40662dcc1e52c7b8f04",
+    "url": "/climax/index.html"
+  }
+];
